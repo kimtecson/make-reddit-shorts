ac415acbf114d1e159f7c1d359fed331377ae5a7
kimberly tecson <tecsonkim@gmail.com>
2024-07-09 00:07:49 +0800

    added create pages

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -1,4 +1,10 @@
 class BatchesController < ApplicationController
   def new
+    @user = current_user
+    @batch = Batch.new
+  end
+  def create
+    @owner = current_user
+    @batch = Batch.new
   end
 end
--- app/controllers/outputs_controller.rb
+++ app/controllers/outputs_controller.rb
@@ -1,4 +1,15 @@
 class OutputsController < ApplicationController
     def new
+      @user = current_user
+      @ouput = Output.new
     end
+    def create
+      @owner = current_user
+      @ouput = Output.new
+    end
+    # def show
+    # @user = current_user
+    # # @sources = Source.all
+    # @ouput = Output.find(params[:id])
+    # end
 end
