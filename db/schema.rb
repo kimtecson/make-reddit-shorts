55824f1bf9ef234a0e896f708c718675dc004a91
kimberly tecson <tecsonkim@gmail.com>
2024-07-09 20:56:11 +0800

    re migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_02_125942) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_02_123511) do
   create_table "batches", force: :cascade do |t|
     t.integer "source_id", null: false
     t.datetime "created_at", null: false
