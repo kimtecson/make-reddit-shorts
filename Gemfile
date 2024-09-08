58aff2906a1741dbdab7bb9eb35b0620bf863fa5
kimberly tecson <tecsonkim@gmail.com>
2024-09-08 17:54:11 +0800

    added meta tags

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source "https://rubygems.org"
 
 ruby "3.1.2"
 
+gem 'meta-tags'
 
 # gems for services (added by stan)
 gem 'open3'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,30 @@
 module ApplicationHelper
+  def default_meta_tags
+    #TO DO: update before going live or once needed details are available
+    # update each html page <% set_meta_tags title: 'Contact page', description: 'Contact us from...' %>
+
+    {
+      site: 'LazyShorts.dev',
+      title: 'Lazy Shorts',
+      reverse: true,
+      separator: '|',
+      description: 'Automatically generating short-form content from Reddit posts.',
+      keywords: 'reddit, short-form content, video creation, auto-generated content, Reddit videos, Reddit clips, quick video generation, video marketing, content creation, social media videos, automated video tools, viral content, trending videos, video automation, Reddit video tool, content repurposing, fast video creation, short videos, video platform, free ai video generator',
+      canonical: request.original_url,
+      noindex: !Rails.env.production?,
+      icon: [
+        { href: image_url('favicon.ico') },
+        { href: image_url('icon.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+      ],
+      og: {
+        site_name: 'LazyShorts.dev',
+        title: 'Lazy Shorts',
+        description: 'Create short-form videos effortlessly from any Reddit post URL.',
+        type: 'website',
+        url: request.original_url,
+        image: image_url('shorts-preview.png')
+      }
+    }
+
+  end
 end
