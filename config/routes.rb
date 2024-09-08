8eb92e3b422d38fc5ca50ee9e62cf084070f183e
dev-stan <mrstanleyzalewski@gmail.com>
2024-09-08 15:19:04 +0200

    fix sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,43 @@
 Rails.application.routes.draw do
+  # Error handling routes
   get 'errors/not_found'
   get 'errors/internal_server_error'
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
+
+  # Devise routes
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-  get 'reddit_post', to: 'reddit_posts#show'
+  # Health check route
+  get 'up', to: 'rails/health#show', as: :rails_health_check
+
+  # Static pages
   get '/tos', to: 'pages#tos'
   get '/privacy', to: 'pages#privacy'
   get '/pricing', to: 'pages#pricing'
   get '/about', to: 'pages#about'
-  root to: "pages#home"
-  post "send_feedback", to: "pages#send_feedback"
-  resources :newsletter_subscribers, only: [:new, :create, :destroy]
-  get 'unsubscribe/:id', to: "newsletter_subscribers#unsubscribe", as: "unsubscribe"
-  match "/404", to: "errors#not_found", via: :all
-  match "/500", to: "errors#internal_server_error", via: :all
+  root to: 'pages#home'
+  
+  # Feedback route
+  post 'send_feedback', to: 'pages#send_feedback'
 
-  # Define routes for outputs
+  # Newsletter subscribers
+  resources :newsletter_subscribers, only: [:new, :create, :destroy]
+  get 'unsubscribe/:id', to: 'newsletter_subscribers#unsubscribe', as: 'unsubscribe'
+
+  # Reddit posts
+  get 'reddit_post', to: 'reddit_posts#show'
+
+  # Outputs and nested schedules
   resources :outputs, only: [:index, :show, :new, :create, :destroy] do
     member do
-      get :progress   # New route to track job progress for a specific output
-      get :video_url # New route for fetching the video URL
+      get :progress   # Job progress tracking
+      get :video_url  # Fetch video URL
     end
     resources :schedules, only: [:create]
   end
 
+  # Schedules and sources
   resources :schedules, only: [:index, :destroy]
-  resources :sources, only: [:create, :new]
+  resources :sources, only: [:new, :create]
 end
